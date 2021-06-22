e9a44d2baa01484716ccdfd5bf6ee10eb2081f98
RosePinkDragon <59683036+RosePinkDragon@users.noreply.github.com>
2021-06-22 21:36:50 +0530

    Added Express And Initiliazed Package

--- index.js
+++ index.js
@@ -1,50 +1,19 @@
-const http = require("http");
-const fs = require("fs");
+const express = require('express')
 
-const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
+const app = express()
 
-  // fs.readFile("./views/about.html", (err, data) => {
-  //   if (err) {
-  //     console.log(err);
-  //     res.end();
-  //   }
-  //   res.write(data);
-  //   // res.end(data);
-  // });
-
-  let path = "./views";
-
-  switch (req.url) {
-    case "/":
-      path += "/index.html";
-      res.statusCode = 200;
-      break;
-    case "/about-me":
-      res.setHeader("Location", "/about");
-      res.statusCode = 301;
-      res.end();
-      break;
-    case "/about":
-      path += "/about.html";
-      res.statusCode = 200;
-      break;
-    default:
-      path += "/404.html";
-      res.statusCode = 200;
-      break;
-  }
-
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end();
-    }
-    // res.write(data);
-    res.end(data);
-  });
+app.get('/',(req, res) => {
+  res.send('Hello World')
 });
 
-server.listen(4000, "localhost", () => {
-  console.log("listening on port 4000");
-});
+app.get('/about', (req, res) => {
+  res.send('About')
+})
+
+app.get('*', (req, res) => {
+  res.send('404 page')
+})
+
+app.listen(3000, ()=>{
+  console.log('Port running on 3000')
+})
---
+++ oldServer.js
@@ -0,0 +1,41 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+
+  let path = "./views";
+
+  switch (req.url) {
+    case "/":
+      path += "/index.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.setHeader("Location", "/about");
+      res.statusCode = 301;
+      res.end();
+      break;
+    case "/about":
+      path += "/about.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "/404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    }
+    // res.write(data);
+    res.end(data);
+  });
+});
+
+server.listen(4000, "localhost", () => {
+  console.log("listening on port 4000");
+});
