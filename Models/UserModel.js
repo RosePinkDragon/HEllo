80155573adcd1eda960a007e32050da6b272a603
RosePinkDragon <59683036+RosePinkDragon@users.noreply.github.com>
2021-07-11 12:52:47 +0530

    Refactored the code
    Refactored the frontend code to look cleaner and better

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,25 +1,26 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bcrypt = require("bcrypt");
+var { isEmail } = require("validator");
 
 const userSchema = new Schema({
   email: {
     type: String,
     required: [true, "Title is required"],
     unique: true,
+    lowercase: true,
+    validate: [isEmail, "Email is Invalid"],
   },
   password: {
     type: String,
-    minLength: 6,
+    minLength: [6, "Min lenght is 6 characters"],
     required: [true, "Password Is required"],
   },
 });
 
 userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt();
-  console.log(this.password);
   this.password = await bcrypt.hash(this.password, salt);
-  console.log(this.password);
   next();
 });
 
@@ -27,6 +28,18 @@ userSchema.post("save", function (doc, next) {
   console.log("User is saved successfully");
   next();
 });
+
+userSchema.statics.login = async function (email, password) {
+  const user = await User.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error("User Not Found");
+  }
+  throw Error("Incorret details");
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,22 @@
 const jwt = require("jsonwebtoken");
 const User = require("../Models/UserModel");
 
+const handleErrors = (err) => {
+  let errors = { email: "", password: "" };
+
+  if (err.code === 11000) {
+    errors.email = "That Email is already Taken";
+  }
+
+  if (err.message.includes("validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
+};
+
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 const createToken = (id) => {
   return jwt.sign({ id }, process.env.SECRET, { expiresIn: maxAge });
@@ -14,21 +30,32 @@ const user_login = (req, res) => {
   res.render("log-in", { title: "Login User" });
 };
 
-const user_create_post = (req, res) => {
-  console.log(req.body);
+const user_create_post = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = User.create({ email, password });
+    const user = await User.create({ email, password });
+    const token = createToken(user._id);
+    res.cookie("BlogCookie", token, { httpOnly: true, maxAge: maxAge });
+    res.status(201).json({ user: user._id });
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
+};
+
+const user_login_post = async (req, res) => {
+  const { email, password } = req.body;
+  console.log(req.body);
+  try {
+    const user = await User.login(email, password);
     const token = createToken(user._id);
     res.cookie("BlogCookie", token, { httpOnly: true, maxAge: maxAge });
     res.status(201).json({ user: user._id });
   } catch (err) {
     console.log(err);
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
   }
-};
-const user_login_post = (req, res) => {
-  console.log(req.body);
-  console.log("User Login here");
 };
 
 const user_log_out = (req, res) => {
